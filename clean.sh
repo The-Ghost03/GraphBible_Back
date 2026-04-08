84cf736c8f3400bea4cc6a92a9917f7d704dd323
Franck Schalôm <75930919+The-Ghost03@users.noreply.github.com>
2026-04-08 22:05:42 +0000

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -6,10 +6,3 @@ with get_db().session() as session:
     session.run(\"MATCH (u:User {email: 'kyoa240@gmail.com'}) DETACH DELETE u\")
 print('✅ Utilisateur kyoa240@gmail.com supprimé avec succès !')
 "
-
-docker exec -it biblegraph_backend python -c "
-from database import get_db
-with get_db().session() as session:
-    session.run(\"MATCH (u:User {email: 'schalom.yao@softskills.ci'}) DETACH DELETE u\")
-print('✅ Utilisateur schalom.yao@softskills.ci supprimé avec succès !')
-"
